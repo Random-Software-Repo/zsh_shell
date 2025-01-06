aa17c05495f2ceb5afb7aae78e22fa0f7cfe8c34
Jack <jack@example.com>
2025-01-06 14:09:03 -0700

    zfsl now is a function, not alias, and shows volumes as well as filesystems. changed the columns to show usedds not used space, and shortened the TYPE column for narrower results.

--- zbm.zfs.zsh
+++ zbm.zfs.zsh
@@ -6,5 +6,11 @@
 ##
 if (( $+commands[zfs] ))
 then
-	alias zfsl="zfs list -t filesystem -o name,avail,used,encryption,canmount,mountpoint"
+	function zfsl()
+	{
+		# shows filesystem and volumes. replaces long 'filesystem' and 'volume' labels with 'fs' 
+		# and 'v' respectively so results aren't as wide.
+		# usedds is the actual spaced used by the fs/vol, not the total spaced used.
+		zfs list -t vol,filesystem -o name,type,avail,usedds,encryption,canmount,mountpoint "$@" |sed 's/filesystem/fs\t/g'|sed 's/TYPE     /TYPE/g'| sed 's/volume/v/g'
+	}
 fi
